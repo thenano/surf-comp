33c9a507e4d58d263696cdee09656221a0efd1f5
Sam Gibson <sam@ifdown.net>
2016-04-03 17:33:51 +1000

    Cleanup athlete model

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,55 +1,34 @@
 class Athlete
   include Mongoid::Document
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, :omniauth_providers => [:facebook]
 
-  ## Database authenticatable
-  field :email,              type: String, default: ''
-  field :encrypted_password, type: String, default: ''
-
-  ## Recoverable
-  field :reset_password_token,   type: String
-  field :reset_password_sent_at, type: Time
-
-  ## Rememberable
-  field :remember_created_at, type: Time
-
-  ## Trackable
-  field :sign_in_count,      type: Integer, default: 0
-  field :current_sign_in_at, type: Time
-  field :last_sign_in_at,    type: Time
-  field :current_sign_in_ip, type: String
-  field :last_sign_in_ip,    type: String
-
-  ## Confirmable
-  # field :confirmation_token,   type: String
-  # field :confirmed_at,         type: Time
-  # field :confirmation_sent_at, type: Time
-  # field :unconfirmed_email,    type: String # Only if using reconfirmable
-
-  ## Lockable
-  # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
-  # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
-  # field :locked_at,       type: Time
-
-  ## Facebook Omniauth
-  field :provider, type: String
   field :uid, type: String
-
   field :name, type: String
   field :image, type: String
+  field :provider, type: String
+  field :email, type: String, default: ''
+  field :encrypted_password, type: String, default: ''
+  field :reset_password_token, type: String
+  field :reset_password_sent_at, type: Time
+  field :current_sign_in_ip, type: String
+  field :last_sign_in_ip, type: String
+  field :sign_in_count, type: Integer, default: 0
+  field :remember_created_at, type: Time
+  field :current_sign_in_at, type: Time
+  field :last_sign_in_at, type: Time
+
+  validates_presence_of :name, :email
 
   has_and_belongs_to_many :heats
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |athlete|
-      athlete.email = auth.info.email
       athlete.password = Devise.friendly_token[0,20]
-      athlete.name = auth.info.name   # assuming the user model has a name
-      athlete.image = auth.info.image # assuming the user model has an image
+      athlete.email = auth.info.email
+      athlete.name = auth.info.name
+      athlete.image = auth.info.image
     end
   end
 end
