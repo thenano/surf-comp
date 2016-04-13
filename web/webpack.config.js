7c5b4e853114b7b58ae20cc57b095ac88eae4e86
Fernando Freire <ffreire@thoughtworks.com>
2016-04-13 18:38:53 +1000

    adding webpack-dev-server for nice deving

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,10 +1,11 @@
-var path = require("path");
+const path = require("path");
+const buildPath = path.join(__dirname, "public");
 
 module.exports = {
-    entry: "./src/site.js",
+    entry: ["./src/site.js", "webpack-dev-server/client?http://localhost:8080"],
 
     output: {
-        path: path.join(__dirname, "public"),
+        path: buildPath,
         filename: "app.js"
     },
 
@@ -12,6 +13,11 @@ module.exports = {
         loaders: [
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
         ]
+    },
+    devServer: {
+        contentBase: buildPath,
+        host: "0.0.0.0",
+        port: 8080
     }
 };
 
