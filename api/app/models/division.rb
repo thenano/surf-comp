8601482c03712dd052b1a608d8e6647c0798325b
Fernando Freire <ffreire@thoughtworks.com>
2016-04-20 20:20:48 +1000

    slight refactoring of draw

--- api/app/models/division.rb
+++ api/app/models/division.rb
@@ -3,26 +3,22 @@ class Division < ApplicationRecord
   has_many :heats, -> { order 'position' }
 
   HEAT_SIZE = 6
-  ROUND_NAMES = %w(Final Semifinal Quarterfinal Round)
+  ROUND_NAMES = %w(Final Semifinal Quarterfinal)
 
   def draw
-    number_of_rounds = Math.log2((users.length.to_f / HEAT_SIZE)).ceil + 1
+    number_of_rounds = Math.log2((users.length.to_f / HEAT_SIZE)).ceil
 
-    round = 1
     remaining_athletes = users.length.to_f
-    heat_position_offset = 0
+    heat_offset = 0
 
-    while number_of_rounds > 0 do
-      round_name = ROUND_NAMES[number_of_rounds - 1]
-      round_name += " #{round}" if (number_of_rounds - 1) > 2
+    (0..number_of_rounds).to_a.reverse.each_with_index do |upper_round, lower_round|
+      round_name = ROUND_NAMES[upper_round] || "Round #{lower_round + 1}"
 
       number_of_heats = (remaining_athletes / HEAT_SIZE).ceil
-      number_of_heats.times { |heat_number| heats << Heat.new({round: round_name, position: heat_position_offset + heat_number}) }
+      number_of_heats.times { |heat_number| heats << Heat.new({round: round_name, position: heat_offset + heat_number}) }
 
       remaining_athletes /= 2.0
-      heat_position_offset += 10
-      number_of_rounds -= 1
-      round += 1
+      heat_offset += 10
     end
 
     number_of_heats = (users.length.to_f / HEAT_SIZE).ceil
