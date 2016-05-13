8f18f0d8ad07a813a0ad1113dd7931e64241df4a
Fernando Freire <ffreire@thoughtworks.com>
2016-05-13 20:56:29 +1000

    last try to get heroku to run these migrations

--- api/app/models/athlete_heat.rb
+++ api/app/models/athlete_heat.rb
@@ -1,6 +1,6 @@
 class AthleteHeat < ApplicationRecord
   belongs_to :heat
-  belongs_to :athlete, class_name: User
+  belongs_to :athlete, class_name: User, foreign_key: 'user_id'
 
   validates_presence_of  :position
 end
