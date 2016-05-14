5ccaeb064c2f1b200df1629193851f9b0f2a082b
Fernando Freire <ffreire@thoughtworks.com>
2016-05-14 11:15:48 +1000

    this one will work!

--- api/db/migrate/20160507095022_create_athlete_heats.rb
+++ api/db/migrate/20160507095022_create_athlete_heats.rb
@@ -2,13 +2,14 @@ class CreateAthleteHeats < ActiveRecord::Migration[5.0]
   def change
     create_table :athlete_heats do |t|
       t.belongs_to :heat, foreign_key: true
+      t.integer :athlete_id
       t.integer :position
 
       t.index [:position, :heat_id], unique: true
+      t.index [:athlete_id, :heat_id], unique: true
 
       t.timestamps
     end
-    add_foreign_key(:athlete_heats, :users, column: :athlete_id)
-    add_index(:athlete_heats, [:athlete_id, :heat_id], unique: true)
+    add_foreign_key :athlete_heats, :users, column: :athlete_id
   end
 end
