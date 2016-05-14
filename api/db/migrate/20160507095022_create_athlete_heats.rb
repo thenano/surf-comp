ae7770b4a6787eae868e7df48a8f223e0c838650
Fernando Freire <ffreire@thoughtworks.com>
2016-05-14 11:13:48 +1000

    last try for heroku

--- api/db/migrate/20160507095022_create_athlete_heats.rb
+++ api/db/migrate/20160507095022_create_athlete_heats.rb
@@ -2,13 +2,13 @@ class CreateAthleteHeats < ActiveRecord::Migration[5.0]
   def change
     create_table :athlete_heats do |t|
       t.belongs_to :heat, foreign_key: true
-      t.belongs_to :user, foreign_key: true, column: :athlete_id
       t.integer :position
 
       t.index [:position, :heat_id], unique: true
-      t.index [:athlete_id, :heat_id], unique: true
 
       t.timestamps
     end
+    add_foreign_key(:athlete_heats, :users, column: :athlete_id)
+    add_index(:athlete_heats, [:athlete_id, :heat_id], unique: true)
   end
 end
