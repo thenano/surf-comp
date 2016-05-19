e2403145d634b37c52446e9bdc36016a8443648b
Fernando Freire <ffreire@thoughtworks.com>
2016-05-19 23:30:05 +1000

    correct division names and seeds for comp 3

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
 
 event = Event.create(name: 'BBR comp 3', date: '2016-22-05')
 
