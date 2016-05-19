a864746b5d34417db95b020c4c8e2e6710270a63
Fernando Freire <ffreire@thoughtworks.com>
2016-05-19 18:23:32 +1000

    the seeding

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -6,21 +6,38 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-event = FactoryGirl.create(:event, name: 'Trial comp', date: '2016-05-14')
-groms = FactoryGirl.create(:division, name: 'Trial crew')
+event = Event.create(name: 'BBR comp 3', date: '2016-22-05')
 
-['Ian Wallace',
-'Felix Ettelson',
-'Lara Demelian',
-'Luke Adam',
-'Archie MacDonald',
-'Finn Fillipek',
-'Jack Hobbs',
-'Andreas Thoma',
-'Saul Hirner',
-'Izzie Cremer',
-'Nic Mcgrath'].each { |athlete| FactoryGirl.create(:user, name: athlete)}
-
-EventDivision.create(event: event, division: groms, users: User.all)
+CSV.foreach('./db/comp3_seeds.csv', headers: true) do |row|
+  division = Division.find_by(name: row['DIVISON'])
+  if division.nil?
+    division = Division.create(name: row['DIVISON'])
+    event_division = EventDivision.create(event: event, division: division)
+  else
+    event_division = EventDivision.find_by(division_id: division.id)
+  end
+  event_division.users.create(password: Devise.friendly_token[0,20],
+              email: row['NAME'].gsub(/\s/, '_') + '@create.com',
+              name: row['NAME'])
+end
 
 event.draw
+
+# event = FactoryGirl.create(:event, name: 'Trial comp', date: '2016-05-14')
+# groms = FactoryGirl.create(:division, name: 'Trial crew')
+#
+# ['Ian Wallace',
+# 'Felix Ettelson',
+# 'Lara Demelian',
+# 'Luke Adam',
+# 'Archie MacDonald',
+# 'Finn Fillipek',
+# 'Jack Hobbs',
+# 'Andreas Thoma',
+# 'Saul Hirner',
+# 'Izzie Cremer',
+# 'Nic Mcgrath'].each { |athlete| FactoryGirl.create(:user, name: athlete)}
+#
+# EventDivision.create(event: event, division: groms, users: User.all)
+#
+# User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
