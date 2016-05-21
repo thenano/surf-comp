d9fbbf229202370b42b580838f2ebd3fe1da5e6d
Sam Gibson <sam@ifdown.net>
2016-05-21 17:50:00 +1000

    Scroll the upcoming heats

--- web/src/components/events/spectate.js
+++ web/src/components/events/spectate.js
@@ -1,6 +1,7 @@
 /* global Pusher */
 
 import React from "react";
+import ReactDOM from "react-dom";
 import Immutable from "immutable";
 import * as EventActions from "../../actions/event";
 import { fetch, liveScores } from "../../decorators";
@@ -8,24 +9,40 @@ import { connect } from "react-redux";
 import { HeatResults } from "./results/card";
 import { JERSEYS } from "../surfing";
 
+const Easing = {
+  // no easing, no acceleration
+  linear: function (t) { return t },
+  // accelerating from zero velocity
+  easeInQuad: function (t) { return t*t },
+  // decelerating to zero velocity
+  easeOutQuad: function (t) { return t*(2-t) },
+  // acceleration until halfway, then deceleration
+  easeInOutQuad: function (t) { return t<.5 ? 2*t*t : -1+(4-2*t)*t },
+  // accelerating from zero velocity 
+  easeInCubic: function (t) { return t*t*t },
+  // decelerating to zero velocity 
+  easeOutCubic: function (t) { return (--t)*t*t+1 },
+  // acceleration until halfway, then deceleration 
+  easeInOutCubic: function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 },
+  // accelerating from zero velocity 
+  easeInQuart: function (t) { return t*t*t*t },
+  // decelerating to zero velocity 
+  easeOutQuart: function (t) { return 1-(--t)*t*t*t },
+  // acceleration until halfway, then deceleration
+  easeInOutQuart: function (t) { return t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t },
+  // accelerating from zero velocity
+  easeInQuint: function (t) { return t*t*t*t*t },
+  // decelerating to zero velocity
+  easeOutQuint: function (t) { return 1+(--t)*t*t*t*t },
+  // acceleration until halfway, then deceleration 
+  easeInOutQuint: function (t) { return t<.5 ? 16*t*t*t*t*t : 1+16*(--t)*t*t*t*t }
+}
+
 var d = React.DOM;
 
 function zeroPad(num, places) {
     var zero = places - num.toString().length + 1;
     return Array(+(zero > 0 && zero)).join("0") + num;
-}
-
-class EmptySlot extends React.Component {
-    render() {
-        let { position } = this.props;
-        let jersey = JERSEYS[position];
-
-        return d.li({className: `empty athlete ${jersey}`}, "waiting for athlete");
-    }
-}
-
-function emptySlot(heat, position) {
-    return React.createElement(EmptySlot, {key: position});
 }
 
 class AthleteSlot extends React.Component {
@@ -52,7 +69,6 @@ class TinyHeat extends React.Component {
             round = heat.get("round"),
             number = heat.get("number"),
             startTime = null;
-            // startTime = heat.get("start_time") ? new Date(heat.get("start_time")) : undefined;
 
         let displayTime;
         if (startTime) {
@@ -82,6 +98,64 @@ function heat(heat) {
         {className: "heat-list-item"},
         React.createElement(TinyHeat, {heat})
     );
+}
+
+class HeatMarquee extends React.Component {
+    renderNextHeats() {
+        let { upcomingHeats } = this.props;
+        let bank1 = upcomingHeats.first(),
+            bank2 = upcomingHeats.last();
+
+        let heats = [];
+        let key = 0;
+        for (var i=0; i < Math.max(bank1.size, bank2.size); i++) {
+            if (bank1.get(i)) {
+                heats.push(
+                    d.div({className: "heat-scroll-point", key: key++}, heat(bank1.get(i)))
+                );
+            }
+
+            if (bank2.get(i)) {
+                heats.push(
+                    d.div({className: "heat-scroll-point", key: key++}, heat(bank2.get(i)))
+                );
+            }
+        }
+
+        return heats;
+    }
+
+    componentDidUpdate() {
+        let start = this.refs.marquee.scrollTop;
+        let points = this.refs.marquee.querySelectorAll(".heat-scroll-point");
+        let next = points[this.props.active];
+        let top = next.offsetTop;
+        let distance = Math.abs(top - start);
+        let i = 0;
+        let duration = 1000;
+        let t = setInterval(() => {
+            if (start > top) {
+                let d = Easing.easeOutCubic(i / duration);
+                this.refs.marquee.scrollTop = start - distance * d
+            } else {
+                let d = Easing.easeOutCubic(i / duration);
+                this.refs.marquee.scrollTop = start + distance * d
+            }
+
+            i += 10;
+            if (i > duration) {
+                this.refs.marquee.scrollTop = top;
+                clearInterval(t);
+            }
+        }, 10);
+    }
+
+    render() {
+        return d.div(
+            {ref: "marquee", className: "upcoming-heats marquee"},
+            this.renderNextHeats(),
+        );
+    }
 }
 
 @fetch((store, r) => {
@@ -115,36 +189,16 @@ function heat(heat) {
     heats: state.events.getIn(["current_heats", parseInt(props.params.id)])
 }))
 export class Spectate extends React.Component {
-    renderNextHeats() {
-        let { upcomingHeats } = this.props;
-        let bank1 = upcomingHeats.first(),
-            bank2 = upcomingHeats.last();
+    constructor(props, context) {
+        super(props, context);
 
-        let heats = [];
-        let key = 0;
-        for (var i=0; i < Math.max(bank1.size, bank2.size); i++) {
-            if (bank1.get(i)) {
-                heats.push(
-                    d.div({key: key++}, heat(bank1.get(i)))
-                );
-            }
-
-            if (bank2.get(i)) {
-                heats.push(
-                    d.div({key: key++}, heat(bank2.get(i)))
-                );
-            }
-        }
-
-        return heats;
-        // let { heats } = this.props;
-        // heats = heats.shift();
-
-        // return heats.map((h, i) => {
-        //     console.log(h);
-        //     return null;
-        //     // return d.div({key: i}, heat(h))
-        // })
+        let heatCount = props.upcomingHeats.get(0).size + props.upcomingHeats.get(1).size;
+        this.state = { top: 0 }
+        setInterval(() => {
+            this.setState({
+                top: (this.state.top + 1) % heatCount
+            });
+        }, 5000);
     }
 
     renderActiveHeats() {
@@ -180,7 +234,6 @@ export class Spectate extends React.Component {
         );
     }
 
-
     render() {
         const { events } = this.props;
         const { heats } = this.props;
@@ -191,16 +244,23 @@ export class Spectate extends React.Component {
             {id: "spectate", className: "heat-wrapper"},
 
             d.div(
-                {className: "display"},
+                {className: "display header"},
+                d.h2({className: "active-heats"}, "Live"),
+                d.h2({className: "upcoming-heats"}, "Upcoming")
+            ),
+
+            d.div(
+                {className: "display main"},
                 d.div(
                     {className: "active-heats"},
-                    d.h2({}, "Live"),
                     this.renderActiveHeats()
                 ),
-                d.div(
-                    {className: "upcoming-heats"},
-                    d.h2({}, "Upcoming"),
-                    this.renderNextHeats(),
+                React.createElement(
+                    HeatMarquee,
+                    {
+                        active: this.state.top,
+                        upcomingHeats: this.props.upcomingHeats
+                    }
                 )
             )
         );
